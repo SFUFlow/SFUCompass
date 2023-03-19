ff6a7d120112d7876050620a787881f26f7b838c
Alexander Li <li.alex.0901@gmail.com>
2023-03-19 15:10:49 -0700

    signup

---
+++ sfucompass/src/pages/user/signup.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+    return (
+      <div className='h-screen w-screen bg-[url(/assets/sfuFountain.webp)] bg-center bg-cover'>
+          <div className="backdrop-blur-md h-screen w-screen">
+              <div >
+  
+              </div>
+  
+          </div>
+      </div>
+    );
+  }
