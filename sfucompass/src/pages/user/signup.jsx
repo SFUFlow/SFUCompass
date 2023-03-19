86b646517f92ccde92c24381f7adfd239dddad10
Alexander Li <li.alex.0901@gmail.com>
2023-03-19 15:30:14 -0700

    basic form

--- sfucompass/src/pages/user/signup.jsx
+++ sfucompass/src/pages/user/signup.jsx
@@ -1,9 +1,11 @@
-export default function Login() {
+import SignUpForm from '../../components/user/SignUpForm';
+
+export default function SignUp() {
     return (
       <div className='h-screen w-screen bg-[url(/assets/sfuFountain.webp)] bg-center bg-cover'>
           <div className="backdrop-blur-md h-screen w-screen">
-              <div >
-  
+              <div className="w-1/3 h-screen bg-white float-right outline outline-2 outline-red-900 drop-shadow-[-10px_35px_25px_rgba(0,0,0,0.3)]">
+                <SignUpForm />
               </div>
   
           </div>
