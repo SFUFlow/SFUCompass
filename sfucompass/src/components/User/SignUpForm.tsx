3b4b4506d78a0030f179d5ad15f7a72062e41d41
Alexander Li <li.alex.0901@gmail.com>
2023-03-19 16:20:25 -0700

    update signup

--- sfucompass/src/components/User/SignUpForm.tsx
+++ sfucompass/src/components/User/SignUpForm.tsx
@@ -1,41 +1,58 @@
+import Link from "next/link";
+import Image from "next/image";
 export default function SignUpForm() {
 
     return (
         <div>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen w-full">
 
-                <div className="w-full">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            KILL ME I WANNA DIE
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                <div className="w-full ">
+                    <h1 className="text-xl font-bold uppercase mb-7 text-gray-900 md:text-2xl">
+                        lorem ipsum
+                    </h1>
+                    <form className="space-y-4 md:space-y-6" action="#" noValidate>
+                        <div>
+                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-500 text-gray-500 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" placeholder="name@mail.com" required />
+                        </div>
+                        <div>
+                            <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" required />
+                        </div>
+                        <div>
+                            <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                            <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" required />
+                        </div>
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-red-800 checked:bg-red-800" />
                             </div>
-                            <div>
-                                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                            <div className="ml-3 text-sm">
+                                <label for="remember" className="text-gray-500">Remember me</label>
                             </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label for="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
-                                    </div>
-                                </div>
-                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                            </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                        </div>
+                        <button type="submit" className="w-full text-white bg-red-900 rounded-md p-2">Sign up</button>
+
+                        <div className="flex items-center justify-between w-full">
+                            <div className="w-full border-b border-gray-500"></div>
+
+                            <div className="mx-4 text-xs text-center text-gray-500 uppercase">or</div>
+
+                            <div className="w-full border-b border-gray-500"></div>
+                        </div>
+
+                        <button type="button" className="w-full text-black outline outline-1 bg-white rounded-md p-2">Sign up with some other shit</button>
+
+                        <div className="flex items-center justify-between">
+                            <p className="text-sm font-light text-gray-500">
+                                Already have an account? <Link href="/user/login" className="font-medium text-red-900 hover:underline dark:text-primary-500">Login</Link>
                             </p>
-                        </form>
-                    </div>
+                            <a href="#" className="text-sm font-medium text-red-900 hover:underline ">Forgot password?</a>
+                        </div>
+                    </form>
                 </div>
+
+                <p>asdfasdf</p>
             </div>
         </div>
     )
