57c1ef1474b3df46e57dc62bcdb4259aacff4290
Alexander Li <li.alex.0901@gmail.com>
2023-03-22 15:58:52 -0700

    clean up handleSubmit

--- sfucompass/src/components/User/SignUpForm.tsx
+++ sfucompass/src/components/User/SignUpForm.tsx
@@ -6,16 +6,24 @@ interface error {
     cause: string;
 }
 
+interface FormElements extends HTMLFormControlsCollection {
+    email: HTMLInputElement;
+    password: HTMLInputElement;
+    confirmPassword: HTMLInputElement;
+}
+
+interface SignUpForm extends HTMLFormElement {
+    readonly elements: FormElements;
+}
+
 export default function SignUpForm() {
 
     const [error, setError] = useState<error | null>(null);
-    
-
 
     const checkPassword = (password: string, confirm: string) => {
         const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&*!]).{8,}$/;
         if (!passwordPattern.test(password)) {
-            throw new Error('Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character',{
+            throw new Error('Password must contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number, and 1 special character', {
                 cause: 'password',
             });
         }
@@ -45,14 +53,21 @@ export default function SignUpForm() {
     }
 
 
-    const handleSubmit = () => {
-        const email = document.querySelector("#email")?.value;
-        const password = document.querySelector("#password")?.value;
-        const confirmPassword = document.querySelector("#confirm-password")?.value;
+    const handleSubmit = (e: React.FormEvent<SignUpForm>) => {
+        // const email = document.querySelector("#email")?.value;
+        // const password = document.querySelector("#password")?.value;
+        // const confirmPassword = document.querySelector("#confirmPassword")?.value;
+        e.preventDefault();
+
+        const email = e.currentTarget.elements.email.value;
+        const password = e.currentTarget.elements.password.value;
+        const confirmPassword = e.currentTarget.elements.confirmPassword.value;
 
         try {
-            isValidEmail(email);
-            checkPassword(password, confirmPassword);
+            console.log(email, password, confirmPassword)
+            // isValidEmail(email);
+            // checkPassword(password, confirmPassword);
+            setError(null);
         } catch (error: any) {
             setError({
                 message: error.message,
@@ -72,28 +87,24 @@ export default function SignUpForm() {
                         </h1>
                         <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
                     </div>
-                    <form className="space-y-4 md:space-y-6" onSubmit={(e) => {
-                        e.preventDefault();
-                        handleSubmit()
-                    }} noValidate>
-
+                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit} noValidate>
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your Email</label>
                             <div className="relative">
                                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                    <svg aria-hidden="true" className={error?.cause == "email" ? "w-5 h-5 text-red-800":"w-5 h-5 text-gray-500 invalid:text-red-800"} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                                    <svg aria-hidden="true" className={error?.cause == "email" ? "w-5 h-5 text-red-800" : "w-5 h-5 text-gray-500 invalid:text-red-800"} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
                                 </div>
-                                <input type="email" id="email" name="email" className={error?.cause == "email" ? "border-red-700 text-red-800 pl-10 p-2.5 bg-gray-50 border rounded-lg focus:ring-red-800 focus:border-red-800 block w-full" : "invalid:border-red-700 invalid:text-red-800 pl-10 p-2.5 bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full"} placeholder="id@sfu.ca" required/>
+                                <input type="email" id="email" name="email" className={error?.cause == "email" ? "border-red-700 text-red-800 pl-10 p-2.5 bg-gray-50 border rounded-lg focus:ring-red-800 focus:border-red-800 block w-full" : "invalid:border-red-700 invalid:text-red-800 pl-10 p-2.5 bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full"} placeholder="id@sfu.ca" required />
                             </div>
                         </div>
 
                         <div>
                             <label for="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className={error?.cause == "password" ? "bg-gray-50 border border-red-700 text-red-800 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" : "bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5"} required/>
+                            <input type="password" name="password" id="password" placeholder="••••••••" className={error?.cause == "password" ? "bg-gray-50 border border-red-700 text-red-800 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" : "bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5"} required />
                         </div>
                         <div>
-                            <label for="confirm-password" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-                            <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className={error?.cause == "password" ? "bg-gray-50 border border-red-700 text-red-800 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" : "bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5"} required />
+                            <label for="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
+                            <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className={error?.cause == "password" ? "bg-gray-50 border border-red-700 text-red-800 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5" : "bg-gray-50 border border-gray-500 text-gray-900 rounded-lg focus:ring-red-800 focus:border-red-800 block w-full p-2.5"} required />
                         </div>
                         <div className="flex items-start">
                             <div className="flex items-center h-5">
