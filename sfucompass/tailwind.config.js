57e916a9f6f8b40c2141868a19c39b456e3501b8
Lepre-CHAU-n <71118130+Lepre-CHAU-n@users.noreply.github.com>
2023-04-11 23:20:50 -0700

    Uninstalled flowbite install daisy UI

--- sfucompass/tailwind.config.js
+++ sfucompass/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-  "./node_modules/flowbite/**/*.js"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [require("daisyui")],
 }
